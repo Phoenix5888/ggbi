6c896ad6a673e4836cfb17beb50f2cab2a2c3ed5
Hanna Liubarska <gvg8say@gmail.com>
2024-10-24 11:45:42 +0300

    next commit

--- gora.js
+++ gora.js
@@ -5,10 +5,11 @@ for (let i=1; i<=t; i++) {
   for (let j=1; j<=t-i; j++) {
     tree+=" "
   }
+  
   for (let k =1; k<=i*2-1; k++){
 tree+="*";
   }
 if (i<t) tree+="\n"
   }
-  
+
   console.log(tree)
